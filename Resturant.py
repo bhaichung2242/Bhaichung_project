cd462322998e37d4cbcc5b2c73a8cd2572c9f82b
Bhaichung Thikepa Lhomi <bhaichung@Bhaichungs-Laptop.local>
2025-01-12 11:22:37 +0545

    change

--- Resturant.py
+++ Resturant.py
@@ -1,12 +1,13 @@
 # Define menu of resturant
 menu = {
-    'Momo': 120,
-    'Pizza': 340,
-    'Burger': 220,
-    'Sandwich': 140,
+    'Veg momo': 110,'Chicken momo': 120,'Buff momo': 130,
+    'Veg pizza': 140,'Chicken pizza': 280,'Mix pizza': 320,
+    'Americano': 110,'Cappuccino': 210,'Latte': 230,'Mocha': 250,
+
+
 }
 print ("Welcome to Bhaichung Restro.")
-print ("Momo: Rs 120 \nPizza: Rs 340 \nBurger: Rs 220 \nSandwich: Rs 140")
+print ("Coffee \n Americano: 110, Cappuccino: 210, Latte: 230, Mochha: 250 \nMomo \n Veg momo: 110, Chicken momo: 120, Buff momo: 130 \nPizza \n Veg pizza: 140, Chicken pizza: 280, Mix pizza: 320 ")
 
 order_total = 0
 
@@ -21,7 +22,7 @@ another_iteam = input("Do you want to order more? Yes/No = ")
 if another_iteam == "Yes":
     iteam2 = input("What do you like to add? = ")
     if iteam2 in menu:
-        order_total = menu[iteam2]
+        order_total += menu[iteam2]
         print(f"{iteam2} is added in your order.")
     else:
         print(f"{iteam2} is not aviable in Bhaichung Restro.")
